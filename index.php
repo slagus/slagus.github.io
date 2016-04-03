7ff356c23ce42fab48a4ae48a70253403c0c437d
Slagus <amanbheley.pcte@gmail.com>
2016-04-04 04:04:06 +0530

    Create index.php
    bootstrap file for slagus wiki

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Slagus Wiki</title>
+<style>
+  body{
+    background:#f2f2f2;
+    color:#333;
+    font-size:14px;
+    font-family:arial;
+  }
+</style>
+</head>
+<body>
+  <center>
+    <h2>Welcome to Slagus wiki on github.</h2>
+    <p>We are in process, once done we will be avaiable here.</p>
+  </center>
+</body>
+</html>
